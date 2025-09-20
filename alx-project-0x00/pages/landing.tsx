6c355a4ba8166b5d4798ecff826c228a285a4536
Simby1 <kimsimby@gmail.com>
2025-09-20 10:58:05 +0100

    add Button component with title and styles props, update landing page to include buttons

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,17 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button";
 
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+
+       <div className="flex flex-col gap-4 mt-8">
+        <Button title="Small Button" styles="bg-blue-500 text-white px-3 py-1 text-sm rounded-sm" />
+        <Button title="Medium Button" styles="bg-green-500 text-white px-4 py-2 text-base rounded-md" />
+        <Button title="Large Button" styles="bg-red-500 text-white px-6 py-3 text-lg rounded-full" />
+      </div>
     </div>
   )
 }
